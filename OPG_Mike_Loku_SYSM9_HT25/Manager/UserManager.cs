87fd801015a44523fb4f7e5db8b54af2c9d5e9cd
mike_loku <mike010219@gmail.com>
2025-10-19 14:26:28 +0200

    Add project files.

---
+++ OPG_Mike_Loku_SYSM9_HT25/Manager/UserManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPG_Mike_Loku_SYSM9_HT25.Manager
+{
+    internal class UserManager
+    {
+    }
+}
